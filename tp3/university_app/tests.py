fd7216499cb619217bdfcfac9cb1fda5504571c6
Houssemeddine-mag <houssem.magra@univ-constantine2.dz>
2025-11-26 00:51:43 +0100

    i

--- tp3/university_app/tests.py
+++ tp3/university_app/tests.py
@@ -1,19 +1,1 @@
 from django.test import TestCase
-from .models import Student
-
-class StudentModelTest(TestCase):
-    def setUp(self):
-        # Update these fields to match your actual Student model
-        self.student = Student.objects.create(
-            name="John Doe",
-            # Add other fields that exist in your Student model
-        )
-
-    def test_student_creation(self):
-        """Test that a student can be created"""
-        self.assertIsNotNone(self.student.id)
-        self.assertEqual(Student.objects.count(), 1)
-
-    def test_student_str(self):
-        """Test the string representation"""
-        self.assertIsNotNone(str(self.student))
