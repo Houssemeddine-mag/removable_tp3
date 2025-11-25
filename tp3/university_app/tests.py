3d0c20caf47405838f78bf92bb4c45f9f4fea68c
Houssemeddine-mag <houssem.magra@univ-constantine2.dz>
2025-11-26 00:47:20 +0100

    g

--- tp3/university_app/tests.py
+++ tp3/university_app/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
-from django.urls import reverse
-from .models import Student, Course, Enrollment
+from .models import Student
 from datetime import date
 
 class StudentModelTest(TestCase):
@@ -19,45 +18,7 @@ class StudentModelTest(TestCase):
         self.assertEqual(self.student.email, "john.doe@example.com")
         self.assertIsNotNone(self.student.id)
 
-
-class CourseModelTest(TestCase):
-    def setUp(self):
-        self.course = Course.objects.create(
-            name="Introduction to Programming",
-            code="CS101",
-            credits=3
-        )
-
-    def test_course_creation(self):
-        """Test that a course can be created"""
-        self.assertEqual(self.course.name, "Introduction to Programming")
-        self.assertEqual(self.course.code, "CS101")
-        self.assertEqual(self.course.credits, 3)
-        self.assertIsNotNone(self.course.id)
-
-
-class EnrollmentModelTest(TestCase):
-    def setUp(self):
-        self.student = Student.objects.create(
-            first_name="Jane",
-            last_name="Smith",
-            email="jane.smith@example.com",
-            date_of_birth=date(1999, 5, 15)
-        )
-        self.course = Course.objects.create(
-            name="Data Structures",
-            code="CS201",
-            credits=4
-        )
-        self.enrollment = Enrollment.objects.create(
-            student=self.student,
-            course=self.course,
-            grade="A"
-        )
-
-    def test_enrollment_creation(self):
-        """Test that an enrollment can be created"""
-        self.assertEqual(self.enrollment.student, self.student)
-        self.assertEqual(self.enrollment.course, self.course)
-        self.assertEqual(self.enrollment.grade, "A")
-        self.assertIsNotNone(self.enrollment.id)
+    def test_student_count(self):
+        """Test that student count is correct"""
+        count = Student.objects.count()
+        self.assertEqual(count, 1)
